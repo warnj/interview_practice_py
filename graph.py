9a61db3d7e6bbec95e5163bdb334f905904f3f7c
Justin Warner <justin.warner@getcruise.com>
2025-01-27 21:06:31 -0800

    add 2 problems

--- graph.py
+++ graph.py
@@ -26,3 +26,43 @@ def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
                 wl.append((nextCell[0], nextCell[1], dist+1))
                 visited.add(nextCell) # important to mark visited as early as possible to pass time limit
     return -1
+
+# https://leetcode.com/problems/accounts-merge/
+def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
+    # {email -> index in accounts} - problem is there could be multiple duplicates with 1 person and they may not find the same first index and it's inefficient to repeat the entire search to combine later
+    # graph, treat emails as nodes and emails within a person are connected all connected (by single edge to the first email in the list), result is the connected components
+    graph = {}  # {email -> [connected emails of the same person]}
+    names = {}  # {email -> name} not complete
+    for a in accounts:
+        names[a[1]] = a[0]
+        if a[1] not in graph:
+            graph[a[1]] = []
+        for j in range(2, len(a)):
+            email = a[j]
+            graph[a[1]].append(email)  # bidirectional edges
+            if email in graph:
+                graph[email].append(a[1])
+            else:
+                graph[email] = [a[1]]
+    # explore connected components
+    result = []
+    visited = set()
+    for email in graph:
+        if email in visited:
+            continue
+        # explore this connected component dfs
+        person = None
+        emails = set()
+        stack = [email]
+        while stack:
+            cur = stack.pop()
+            visited.add(cur)
+            if cur in names:
+                person = names[cur]
+            emails.add(cur)
+            for child in graph[cur]:
+                if child not in visited:
+                    stack.append(child)
+        emails = sorted(list(emails))
+        result.append([person] + emails)
+    return result
